bbf1ee3ec9e190da8a45349eaf049faba534d176
Aleksey <alekseichubukov@gmail.com>
2016-11-30 00:58:01 +0300

    Receiver-transmitter Arduino example.

---
+++ src/draft/receiver-transmitter/emitter/emitter.ino
@@ -0,0 +1,20 @@
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+int out1=4444;
+int out2=6996;
+
+RF24 radio(9,10);
+const uint64_t pipe00 = 0xE8E8F0F0E1LL;
+void setup() {
+ Serial.begin(57600);
+radio.begin();
+radio.openWritingPipe(pipe00);
+}
+
+void loop() {
+  radio.write(&out1,sizeof(out1));
+  radio.write(&out2,sizeof(out2));
+}
+
---
+++ src/draft/receiver-transmitter/receiver/receiver.ino
@@ -0,0 +1,30 @@
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(9, 10);
+const uint64_t pipe00 = 0xE8E8F0F0E1LL;
+
+void setup() {
+  Serial.begin(57600);
+  radio.begin();
+
+  radio.openReadingPipe(1, pipe00);
+  radio.startListening();
+}
+
+void loop() {
+  int in;
+
+  if (radio.available()) {
+    radio.read(&in, sizeof(in));
+    if (in==4444){
+      Serial.println("found!");
+    }
+    else if(in) {
+      Serial.println(in);
+    }
+  }
+}
+
